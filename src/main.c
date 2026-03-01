c21cdcb16cc81783bd449493e1dc051a877c985e
Tello-P <perezgarrote.tello@gmail.com>
2026-03-01 16:37:56 +0100

    fix:binary search didnt work properly, just use lineal search

--- src/main.c
+++ src/main.c
@@ -33,6 +33,46 @@ Chunk_List freed_chunks = {
     [0] = {.start=heap, .size = sizeof(heap)}
   },
 };
+Chunk_List tmp_chunks={0};
+
+void chunk_list_insert(Chunk_List *list, void *start, size_t size){
+  assert(list->count < CHUNK_LIST_CAPACITY);
+  //insert at end
+  list->chunks[list->count].start = start;
+  list->chunks[list->count].size = size;
+  (void) start;
+
+  for (size_t i=list->count; i>0 && list->chunks[i].start < list->chunks[i-1].start; --i){
+    heap_chunk t = list->chunks[i];
+    list->chunks[i] = list->chunks[i-1];
+    list->chunks[i-1] = t;
+
+  }
+  list->count += 1;
+  
+
+}
+
+
+
+void chunk_list_merge( Chunk_List *dst, const Chunk_List *src){
+  dst->count=0;
+
+  for(size_t i=0; i<src->count; ++i){
+    const heap_chunk chunk = src->chunks[i];
+
+    if (dst->count>0){
+      heap_chunk *top_chunk = &dst->chunks[dst->count-1];
+      if(top_chunk->start + top_chunk->size == chunk.start){
+        top_chunk->size += chunk.size;
+      } else{
+        chunk_list_insert(dst, chunk.start, chunk.size);
+      }
+    } else{
+      chunk_list_insert(dst, chunk.start, chunk.size);
+    }
+  }
+}
 
 
 void chunk_list_dump(const  Chunk_List *list){
@@ -54,6 +94,13 @@ int chunk_start_comp(const void *a, const void *b){
 
 int chunk_list_find(const Chunk_List *list, void *ptr){
 
+  for(size_t i=0; i<list->count; ++i){
+    if (list->chunks[i].start == ptr){
+      return (int)i;
+    }
+  }
+  return -1;
+  /*
   heap_chunk key = {
     .start = ptr
   };
@@ -73,27 +120,8 @@ int chunk_list_find(const Chunk_List *list, void *ptr){
 
 
   return -1;
-
+*/
 }
-
-void chunk_list_insert(Chunk_List *list, void *start, size_t size){
-  assert(list->count < CHUNK_LIST_CAPACITY);
-  //insert at end
-  list->chunks[list->count].start = start;
-  list->chunks[list->count].size = size;
-  (void) start;
-
-  for (size_t i=list->count; i>0 && list->chunks[i].start < list->chunks[i-1].start; --i){
-    heap_chunk t = list->chunks[i];
-    list->chunks[i] = list->chunks[i-1];
-    list->chunks[i-1] = t;
-
-  }
-  list->count += 1;
-  
-
-}
-
 void chunk_list_remove(Chunk_List *list, size_t index){
   assert(index < list->count);
   for (size_t i = index; i<list->count-1; ++i){
@@ -107,6 +135,8 @@ void chunk_list_remove(Chunk_List *list, size_t index){
 void *heap_alloc(size_t size){
 
   if (size > 0){
+    chunk_list_merge(&tmp_chunks, &freed_chunks);
+    freed_chunks = tmp_chunks;
   for (size_t i=0; i<freed_chunks.count; ++i){
     const heap_chunk chunk = freed_chunks.chunks[i];
     if (chunk.size >= size){
@@ -131,6 +161,7 @@ void heap_free(void *ptr){
   if (ptr != NULL){
     const int index = chunk_list_find(&allocated_chunks, ptr);
     assert(index >= 0);
+    assert(ptr == allocated_chunks.chunks[index].start);
     chunk_list_insert(&freed_chunks, allocated_chunks.chunks[index].start,
                       allocated_chunks.chunks[index].size);
     chunk_list_remove(&allocated_chunks, (size_t) index);
@@ -148,21 +179,24 @@ void heap_collect(void *ptr){
 }
 
 
+#define N 10
+
+void *ptrs[N] = {0};
+
 int main(){
   
 
-  for (int i=0; i<10; ++i){
-    void *p = heap_alloc(i);
-    if (i%2 == 0){
-      heap_free(p);
+  for (int i=0; i<N; ++i){
+    ptrs[i] = heap_alloc(i);
+  }
+
+  for (int i=0; i<N; ++i){
+    if (i%2==0){
+      heap_free(ptrs[i]);
     }
   }
 
-heap_alloc(420);
-
-  for (int i=0; i <= 4; ++i){
-    heap_alloc(i);
-  }
+  heap_alloc(8);
 
 
   chunk_list_dump(&allocated_chunks);
